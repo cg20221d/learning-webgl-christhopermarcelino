623a8fe33bf0ecc2ec403dd061793a4a18a292ed
christhopermarcelino <christhopermarcelino@gmail.com>
2022-09-07 15:20:25 +0700

    feat: set up webgl to canvas

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-function main() {}
+function main() {
+  const canvas = document.getElementById("canvas");
+  const gl = canvas.getContext("webgl");
+
+  // vertex shader
+  const vertexShaderCode = "void main() {}";
+  const vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShaderObject, vertexShaderCode);
+  gl.compileShader(vertexShaderObject); // .o file
+
+  // fragment shader
+  const fragmentShaderCode = `
+  void main() {
+
+  }`;
+  const fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
+  gl.compileShader(fragmentShaderObject); // .o file
+
+  const shaderProgram = gl.createProgram(); // .exe file
+  gl.attachShader(shaderProgram, vertexShaderObject);
+  gl.attachShader(shaderProgram, fragmentShaderObject);
+  gl.linkProgram(shaderProgram);
+  gl.useProgram(shaderProgram);
+
+  gl.clearColor(0, 0, 0.5, 1); // navy color rgba
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
