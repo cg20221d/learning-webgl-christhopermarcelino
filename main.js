2ef441d3305ce288fdb44f26fe6382380ba906e4
christhopermarcelino <christhopermarcelino@gmail.com>
2022-09-14 14:52:31 +0700

    feat: draw a dot

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
+/** @type {HTMLCanvasElement} */
+
 function main() {
   const canvas = document.getElementById("canvas");
   const gl = canvas.getContext("webgl");
 
   // vertex shader
-  const vertexShaderCode = "void main() {}";
+  const vertexShaderCode = `
+  void main() {
+    float x = 0.0;
+    float y = 0.0;
+    float z = 0.0;
+    gl_PointSize = 10.0;
+    gl_Position = vec4(x, y, z, 1.0);
+  }
+  `;
+
   const vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertexShaderObject, vertexShaderCode);
   gl.compileShader(vertexShaderObject); // .o file
 
   // fragment shader
   const fragmentShaderCode = `
+  precision mediump float;
   void main() {
-
+    float r = 0.0;
+    float g = 0.0;
+    float b = 1.0;
+    gl_FragColor = vec4(r, g, b, 1.0);
   }`;
   const fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
   gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
@@ -23,6 +38,8 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
-  gl.clearColor(0, 0, 0.5, 1); // navy color rgba
+  gl.clearColor(1.0, 0.65, 0.0, 1.0); // navy color rgba
   gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.drawArrays(gl.POINTS, 0, 1);
 }
